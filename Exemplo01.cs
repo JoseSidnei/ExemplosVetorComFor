66c5609a247be3ce9cf562e5c8fa4c0a56cf7a50
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-04-26 16:50:47 -0300

    Exemplos vetor com for

---
+++ Exemplo01.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemplosVetorComFor
+{
+    public class Exemplo01
+    {
+        public Exemplo01()
+        {
+            string[] mortesDoFilme = new string[5];
+            // forma old
+            // mortesDoFilme[0] = "tony Stark";
+
+            // ARMAZENA AS INFORMAÇÔES NO VETOR
+            for (int i = 0; 1 < mortesDoFilme.Length; i++)
+            {
+                Console.Write("Digite o nome dapessoa que morre no filme: ");
+                mortesDoFilme[i] = Console.ReadLine();
+            }
+
+            Console.Clear();
+            Console.WriteLine("Pessoas Cadastradas: ");
+            // APRESENTA AS INFORMAÇÔES DO VETOR
+            for (int i = 0; i < mortesDoFilme.Length; i++)
+            {
+                Console.WriteLine("Nome: " + mortesDoFilme[i]);
+            }
+
+        }
+    }
+}
---
+++ Exemplo02.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemplosVetorComFor
+{
+    public class Exemplo02
+    {
+        public Exemplo02()
+        {
+            int[] levelNoLOL = new int[5];
+            for (int i = 0; i < levelNoLOL.Length; i++)
+            {
+                Console.Write("Digiteo seu level no LOL: ");
+                levelNoLOL[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int somaLevels = 0;
+
+            for (int i = 0; i < levelNoLOL.Length; i++)
+            {
+                 somaLevels = levelNoLOL[i] + somaLevels;
+            }
+
+            double mediaLevel = somaLevels / levelNoLOL.Length;
+        }
+    }
+}
